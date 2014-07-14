fabe5a368045376e770aa90e6e3cfeeeaa8733db
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-14 21:43:05 +0200

    Removed css associated with index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def after_sign_in_path_for(resource)
+		user_path
 		# redirect to user show after sign in
 	end
 
