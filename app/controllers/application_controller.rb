be3318631d54b11d9653a24f6001efab785735f5
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-14 22:42:26 +0200

    Sanitize user parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # add new strong parameters to sign up
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :username) }
+  end
 end
