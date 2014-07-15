ae4fbdb5049801d52adbaecb7b90f4a04fef2899
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-15 20:32:13 +0200

    Moved user helper methods to customer

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,11 @@
 module CustomersHelper
+	# return customers's beacon count
+	def beacon_count
+		self.beacons.count
+	end
+
+	# return customers's installation count
+	def installation_count
+		self.installations.count
+	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-	# return current user's beacon count
-	def beacon_count
-		current_user.beacons.count
-	end
-
-	# return current user's installation count
-	def installation_count
-		current_user.installations.count
-	end
 end
