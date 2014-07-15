24a7c973eabd4ffbbc73a4a61ce54fce2f5abe03
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-15 15:44:56 +0200

    Added redirect to show page after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+  	user_path(current_user)
+  end
+
   # add new strong parameters to sign up
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:first_name, :last_name, :username) }
