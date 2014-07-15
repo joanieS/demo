d3c1cbb917b02f5ff710dd2c79c12d41a7377588
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-15 17:37:03 +0200

    Added major id attribute to beacons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715145521) do
+ActiveRecord::Schema.define(version: 20140715153254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20140715145521) do
     t.datetime "content_image_updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "major_id"
   end
 
   create_table "customers", force: true do |t|
