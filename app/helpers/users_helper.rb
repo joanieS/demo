d735b77b16b5df7ada4e86a6b9d9d54bccb94f20
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-15 19:13:10 +0200

    Updated account page. Added user helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	# return current user's beacon count
+	def beacon_count
+		current_user.beacons.count
+	end
+
+	# return current user's installation count
+	def installation_count
+		current_user.installations.count
+	end
+
 end
