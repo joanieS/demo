bb22f608fdc59462eb545e0b4c2ca6f972ce994f
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-15 14:24:35 +0200

    Fixed installation show page to list all beacons

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -5,6 +5,7 @@ class BeaconsController < InheritedResources::Base
     @beacon = Beacon.new(beacon_params)
     # installation local variable is not set here. Following code pasted into _form view to make it work
     @installation = Installation.find(params[:installation_id])
+    @beacon.installation_id = @installation.id
     if @beacon.save
       # redirected to beacon show page on successful save
       redirect_to installation_beacon_path(@installation, @beacon), notice: 'Beacon was successfully created.'
