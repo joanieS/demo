1dfefb65f14b00754657acdf64e0969a192dd04d
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-16 14:46:46 +0200

    Removed customer index page. Updated customer routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
-  # GET /customers
-  # GET /customers.json
-  def index
-    @customers = Customer.all
-  end
-
   # GET /customers/1
   # GET /customers/1.json
   def show
