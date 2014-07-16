07b178a0d09af74b7a29cb61e931cff7c9dcb165
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-16 13:13:38 +0200

    Fixed account page edit link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_customer
 
   # user redirected to user show page after sign in
   def after_sign_in_path_for(resource)
@@ -16,9 +15,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation) }
   end
-
-  private
-    def set_customer
-      @customer = Customer.find(current_user.customer_id)
-    end
 end
