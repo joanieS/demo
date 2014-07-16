f3e0404829eb3671dafce4c69cf9ad3f19f66152
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-16 14:50:25 +0200

    Cleaned up customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,12 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
-  # GET /customers/1
-  # GET /customers/1.json
-  def show
-  end
+  def show; end
 
-  # GET /customers/new
   def new
     @customer = Customer.new
   end
 
-  # GET /customers/1/edit
-  def edit
-  end
-
-  # POST /customers
-  # POST /customers.json
   def create
     @customer = Customer.new(customer_params)
 
@@ -31,8 +21,8 @@ class CustomersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
+  def edit; end
+
   def update
     respond_to do |format|
       if @customer.update(customer_params)
@@ -45,8 +35,6 @@ class CustomersController < ApplicationController
     end
   end
 
-  # DELETE /customers/1
-  # DELETE /customers/1.json
   def destroy
     @customer.destroy
     respond_to do |format|
@@ -56,12 +44,11 @@ class CustomersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_customer
       @customer = Customer.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
       params.require(:customer).permit(:name, :category)
     end
