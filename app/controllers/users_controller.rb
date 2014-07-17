c6e59b1ba6dc7db936c807a1fb99b405fbebb30d
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-17 21:37:50 +0200

    Restricted user edit and destroy permissions and changed redirect to after update and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
 
-  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   before_action :set_customer
 
   def index
+    @user = current_user
     @users = User.where(customer_id: current_user.customer_id)
   end
 
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -30,9 +31,10 @@ class UsersController < ApplicationController
   end
 
   def update
+    raise SecurityTransgression unless current_user == @user
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -42,6 +44,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    raise SecurityTransgression unless current_user == @user
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
@@ -52,7 +55,7 @@ class UsersController < ApplicationController
   private
 
     def set_user
-      @user = current_user
+      @user = User.find(params[:id])
     end
   
     def set_customer
