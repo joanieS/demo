235dc2e092cde3344e4fe9d816fe262196645dcd
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-17 19:22:12 +0200

    Removed devise no customer id rerouting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirection, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -53,10 +53,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def redirection
-
-    end
   
     def set_customer
       @customer = Customer.find(current_user.customer_id)
