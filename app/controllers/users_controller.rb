678fd2b877c3830d733a4052a998142f343e88f7
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-17 21:39:55 +0200

    Added user destory route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
     raise SecurityTransgression unless current_user == @user
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   # ensures user interracts with model when editing instead of devise
-  resources :users, only: [:index, :update, :edit]
+  resources :users, only: [:index, :update, :edit, :destroy]
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
