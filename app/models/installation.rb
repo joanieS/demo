64433c8ab8c403ae37a51663fb2f65e5ff21faf4
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-18 23:50:11 +0200

    Updated attr_accessible for installation model

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -2,5 +2,5 @@ class Installation < ActiveRecord::Base
   belongs_to :customer
   has_many :beacons, dependent: :destroy
 
-  
+  attr_accessible :name, :group, :customer_id, :created_at, :updated_at, :active  
 end
