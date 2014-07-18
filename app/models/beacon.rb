60607f5bea67e4065106947da64ebf26758b2871
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-18 23:51:42 +0200

    Cleaned up beacon model

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -1,15 +1,11 @@
 class Beacon < ActiveRecord::Base
 	belongs_to :installation
+  has_many :photos
+  accepts_nested_attributes_for :photos
+  has_many :audio_clips
+  accepts_nested_attributes_for :audio_clips
+  has_attached_file :content_image
+  validates_attachment_content_type :content_image, :content_type => /.+\/.*\Z/
 
   attr_accessible :minor_id, :major_id, :latitude, :longitude, :uuid, :active, :content, :content_type, :audio, :installation_id, :created_at, :updated_at, :content_image_file_name, :content_image_content_type, :content_image_file_name, :content_image_updated_at
-
-	has_many :photos
-	accepts_nested_attributes_for :photos
-
-	has_many :audio_clips
-	accepts_nested_attributes_for :audio_clips
-
-	has_attached_file :content_image
-
-	validates_attachment_content_type :content_image, :content_type => /.+\/.*\Z/
 end
