86aa68c996cddc90bb825b13e7092e5cd445517e
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-18 22:37:35 +0200

    Added Forbidden class in applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  class Forbidden < StandardError; end
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
