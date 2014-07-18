9ff024d71515e43d06e1bcf92bd93d2354ad83ef
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-18 23:48:28 +0200

    Cleaned up customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,6 @@
 class Customer < ActiveRecord::Base
   has_many :users
-
   has_many :installations
-
   has_many :beacons, through: :installations
 
   attr_accessible :name, :category, :created_at, :updated_at
--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,4 +1,6 @@
 class Installation < ActiveRecord::Base
   belongs_to :customer
   has_many :beacons, dependent: :destroy
+
+  
 end
