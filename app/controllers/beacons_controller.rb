d528c2258d4d733199be048ee3a67131e776f2ae
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-20 17:03:44 +0200

    Added image content to beacons strong params

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -66,7 +66,7 @@ class BeaconsController < InheritedResources::Base
   def beacon_params
     params.require(:beacon).permit(
       :minor_id, :major_id, :latitude, :longitude, :content, :content_type, 
-      :audio, :content_image, :uuid, :active
+      :audio, :content_image, :uuid, :active, :image_content
     )
   end
 
