4991e3822c014161c5eca6b32b9d57c2c4c3c8a4
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-20 14:07:15 +0200

    Added user destroy dependency

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
-  has_many :users
+  has_many :users, dependent: :destroy
   has_many :installations, dependent: :destroy
   has_many :beacons, through: :installations
 end
