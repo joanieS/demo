a6a841f72305460cd17981460e4cf81c306af40e
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-21 21:38:56 +0200

    Updated customer form with coordinates and moved coord helper methods to applications controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,14 @@ module ApplicationHelper
     object.active ? "active" : "inactive"    
   end
 
+  def cardinalized_latitude(beacon)
+    beacon.latitude > 0 ? raw("#{beacon.latitude}&deg; N") : raw("#{beacon.latitude.abs}&deg; S")
+  end
+
+  def cardinalized_longitude(beacon)
+    beacon.longitude > 0 ? raw("#{beacon.longitude}&deg; E") : raw("#{beacon.longitude.abs}&deg; W")
+  end
+
   # Paths
 
     # Installation paths
