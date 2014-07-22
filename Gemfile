0df824dc07a3565f432b95aed5cb5bcb5cadb5ca
Hans F. Kassier <hans.kassier@yale.edu>
2014-07-22 10:28:27 +0200

    Annotated models with schema info using gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development do
   gem 'quiet_assets'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'annotate', ">=2.6.0"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  category        :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  activation_code :string(255)      default(""), not null
+#  latitude        :float
+#  longitude       :float
+#
+
 class Customer < ActiveRecord::Base
   has_many :users, dependent: :destroy
   has_many :installations, dependent: :destroy
