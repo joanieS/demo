364b2003b3b109d41872ba8404f9b732d1c90d6c
Hans F. Kassier <hans.kassier@yale.edu>
2014-08-06 03:20:19 +0200

    Added new validations for beacon content

--- app/models/beacon.rb
+++ app/models/beacon.rb
@@ -40,10 +40,9 @@ class Beacon < ActiveRecord::Base
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
 
-  validates_attachment_content_type :content_image, :content_type => /.+\/.*\Z/
+  validates_attachment_content_type :content_image, :content_type => ['video/mp4', 'image/png', 'image/jpeg', 'image/jpg', 'image/gif']
 
   has_attached_file :audio
-
   validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 
   has_attached_file :content
