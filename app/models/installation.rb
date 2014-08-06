1f9521b03e3ceab181594bc17f8c660e30f5e9ff
Hans F. Kassier <hans.kassier@yale.edu>
2014-08-06 19:27:15 +0200

    Updates installation model with image attachment

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -14,4 +14,15 @@
 class Installation < ActiveRecord::Base
   belongs_to :customer
   has_many :beacons, dependent: :destroy
+
+  has_attached_file :image,
+    bucket: ENV['S3_BUCKET_NAME'],
+    s3_credentials: {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
+  validates_attachment_content_type :content_image, :content_type => [
+    'image/png', 'image/jpeg', 'image/jpg', 'image/gif'
+  ]
 end
