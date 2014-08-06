36a5861de1243d76e4df670de5fe4a3591fb8901
Hans F. Kassier <hans.kassier@yale.edu>
2014-08-06 16:47:36 +0200

    Fixed beacon form labels

--- app/helpers/beacons_helper.rb
+++ app/helpers/beacons_helper.rb
@@ -1,23 +1,23 @@
 module BeaconsHelper
 
   def display_id(beacon_id)
-    beacon_id ? beacon_id : "No ID."
+    beacon_id ? beacon_id : "None."
   end
 
   def display_beacon_content_type(beacon_content_type)
-    beacon_content_type ? beacon_content_type : "No content."
+    beacon_content_type ? beacon_content_type : "None."
   end
 
   def display_beacon_content(beacon)
     if beacon.content_type == "image" || beacon.content_type == "local-video"
-      beacon.content_image_file_name ? beacon.content_image_file_name : "No content."
+      beacon.content_image_file_name ? beacon.content_image_file_name : "None."
     elsif beacon.content_type == "web" || beacon.content_type == "web-video"
-      beacon.content_url ? beacon.content_url : "No content."
+      beacon.content_url ? beacon.content_url : "None."
     end
   end
 
   def display_audio(beacon_audio)
-    beacon_audio ? beacon_audio : "No audio file."
+    beacon_audio ? beacon_audio : "None."
   end
 
 end
