d5aa33e81fee84a0d1f7875271a55d92e1730eb5
Hans F. Kassier <hans.kassier@yale.edu>
2014-08-06 12:35:05 +0200

    Updated beacon show page with relevant content description

--- app/helpers/beacons_helper.rb
+++ app/helpers/beacons_helper.rb
@@ -4,8 +4,16 @@ module BeaconsHelper
     beacon_id ? beacon_id : "No ID."
   end
 
-  def display_beacon_content(beacon_content)
-    beacon_content ? beacon_content : "No content."
+  def display_beacon_content_type(beacon_content_type)
+    beacon_content_type ? beacon_content_type : "No content."
+  end
+
+  def display_beacon_content(beacon)
+    if beacon.content_type == "image" || beacon.content_type == "local-video"
+      beacon.content_file_name ? beacon.content_file_name : "No content."
+    elsif beacon.content_type == "web" || beacon.content_type == "web-video"
+      beacon.content_url ? beacon.content_url : "No content."
+    end
   end
 
   def display_audio(beacon_audio)
