d7845f5214e68214c307ab621fee01ec46a7c8bf
Colin Soleim <soleimc@gmail.com>
2014-08-14 14:58:43 -0400

    CSS fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
 
 
   config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
+  config.assets.precompile += %w[ stylesheets/*.css ]
+  config.assets.precompile += %w[ *.css ]
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
