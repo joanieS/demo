9b36b2eb471d6504f69a9fe502fb620a52ab5e92
Hans F. Kassier <hans.kassier@yale.edu>
2014-09-03 20:57:30 -0400

    Adds images and uncomments dev env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,14 +35,14 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # config.paperclip_defaults = {
-    # :storage => :s3,
-    # :s3_credentials => {
-    #   :bucket => 'lufthouseimages',
-    #   :access_key_id => Rails.application.secrets.AWS_ACCESS_KEY_ID,
-    #   :secret_access_key => Rails.application.secrets.AWS_SECRET_ACCESS_KEY
-    # }
-  #}
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'lufthouseimages',
+      :access_key_id => Rails.application.secrets.AWS_ACCESS_KEY_ID,
+      :secret_access_key => Rails.application.secrets.AWS_SECRET_ACCESS_KEY
+    }
+  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
