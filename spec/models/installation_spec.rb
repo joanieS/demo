79e2cfea20f6e6c0176403d27f12d66c2e53df16
joanieS <joan.e.soskin@gmail.com>
2015-01-28 16:57:21 -0500

    more tests

--- spec/models/installation_spec.rb
+++ spec/models/installation_spec.rb
@@ -3,14 +3,14 @@ require 'pry'
 
 RSpec.describe Installation, :type => :model do
 
-  it "has none to begin with" do
-  	expect(Installation.count).to eq 0
-  end
+  # it "has none to begin with" do
+  # 	expect(Installation.count).to eq 0
+  # end
 
-  it "has one when you add one" do
-  	create(:installation)
-  	expect(Installation.count).to eq 1
-  end
+  # it "has one when you add one" do
+  # 	create(:installation)
+  # 	expect(Installation.count).to eq 1
+  # end
 
   it "has a valid factory" do
     expect(create(:installation)).to be_valid
